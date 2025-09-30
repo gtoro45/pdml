43cf23ab4518c7543c922710e220b5c785a442c7
Gabriel <gabrieltoro45@tamu.edu>
2025-09-30 00:41:46 -0500

    threading skeleton complete for format+extract code

--- src/format.c
+++ src/format.c
@@ -10,26 +10,41 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <sched.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <errno.h>
 
+/************************ General Helper Functions ************************/
 
+/************************ Zeek Extraction and Processing ************************/
 // Define the input CSV queue (filepaths to csvs are FIFO's here after formatting)
 char** input_queue;
 
-
-void* format(char* pcap_path) {
+void format(char* pcap_path) {
     // temp function
-    int i = 0;
-    while(pcap_path[i] != '\0') {
-        printf("%c", pcap_path[i]);
-        i++;
-    }
-    printf("\n");
+    printf("%s\n", pcap_path);
 
+    //TODO: FORMAT THEN FIFO
 }
 
-void* extract(char* pcap_path) {
+void* extract(void* arg) {
+    // Cast arg back to cstring
+    char* pcap_path = (char*)arg;
+
     // Check Scheduler CPU affinity
     printf("Thread running on CPU %d\n", sched_getcpu());
+
+    // create a unique logfile ID so that both parent and child have access to the same ID
+    // djb2 hashing algorithm
+    char* tmp = pcap_path;      // used so that actual PCAP file path is not changed by advancing the pointer
+    unsigned long hash = 5381;
+    int c;
+    while(c = *tmp++) { hash = ((hash << 5) + hash) + c; }
+    unsigned long thread_id = hash;
+    
 
     // Use execvp to call zeek extraction process (fork() + execvp())
     pid_t pid = fork();
@@ -40,16 +55,27 @@ void* extract(char* pcap_path) {
     }
     // Child process
     else if(pid == 0) {
+        //create the log file folder using the global thread ID
+        char logdir[128];
+        snprintf(logdir, sizeof(logdir), "./logs/%ld", thread_id);
+        if(mkdir(logdir, 0777) < 0 && errno != EEXIST) {
+            perror("mkdir failed");
+            exit(1);
+        }
+        char logdir_arg[256];
+        snprintf(logdir_arg, sizeof(logdir_arg), "Log::default_logdir=%s", logdir);
+        
         //call zeek script with execvp()
         char* cmd = "/usr/local/zeek/bin/zeek";
-        char* argv[3];
+        char* argv[6];
         argv[0] = cmd;
         argv[1] = "-r";
         argv[2] = pcap_path;
         argv[3] = "extract.zeek";
-        argv[4] = NULL; 
+        argv[4] = logdir_arg;
+        argv[5] = NULL; 
         if(execvp(cmd, argv) < 0) {     //execvp being called here inside if statement to check for failure
-            perror("zeek extraction process exec failed");
+            perror("Zeek extraction process exec failed");
             exit(1); 
         }
     }
@@ -57,68 +83,50 @@ void* extract(char* pcap_path) {
     else {
         //wait for child process to finish
         wait(NULL);
-        printf("zeek extraction completed\n");
+        printf("\nZeek extraction completed: logs stored in logs/%ld\n", thread_id);
 
         //call formatting function
         format(pcap_path);
     }
 }
 
-/**
- * creates and pins a thread to an available CPU core
- * @param *thread: pointer to the thread to be created
- * @param *subroutine: the subroutine to be called
- * @param *arg: the data type or struct containing the subroutine arguments
- * @param *cpuset: the system's cpuset
- */
-int create_and_pin_thread(pthread_t* thread, void* (*subroutine)(void *), void *arg, int nproc, int* cpus, int cpu_count) {
-    // make sure CPU does not fall outside of nproc
-    for(int cpu = 0; cpu < cpu_count; cpu++) {
-        if(cpus[cpu] < 0 || cpus[cpu] > nproc - 1) {
-        perror("Desired CPU affinity exceeds available cores on system");
-        exit(1);
-    }
-    }
-    
-    // thread dependencies
-    cpu_set_t cpuset;
-    CPU_ZERO(&cpuset);
-    for (int i = 0; i < cpu_count; i++) { CPU_SET(i, &cpuset); }
-    
-    // create the thread
-    if(pthread_create(thread, NULL, subroutine, arg) != 0) {
-        perror("Thread creation failure");
-        exit(1);
-    }
 
-    // set thread affinity
-    if(pthread_setaffinity_np(*thread, sizeof(cpu_set_t), &cpuset) != 0) {
-        perror("Thread affinity failure");
-        exit(1);
-    }
-    
-    return 0;
-}
-
+/************************ Main Program ************************/
 int main() {
-    char* pcap_path = "../benign_1_min/Node 1 (Child 3)/node-child3-1757872962.pcap";
+    char* pcap_path1 = "../benign_1_min/Node 1 (Child 3)/node-child3-1757872962.pcap";
+    char* pcap_path2 = "../benign_1_min/Node 2 (Child 4)/fixed_node-child4-1757872963.pcap";
+    char* pcap_path3 = "../benign_1_min/Camera Pod/camera-usb-camera-two-1757872962.pcap";
+    char* pcap_path4 = "../benign_1_min/NGINX Pod/webserver-nginx-5c5d44f994-hswwh-1757872964.pcap";
+    char* pcap_path5 = "../benign_1_min/LiDAR Pod/lidar-usb-lidar-1757872963.pcap";
+    
+
+    // detect environment
+    long nprocs = sysconf(_SC_NPROCESSORS_ONLN);
 
     // create threads
     pthread_t thread1;
     pthread_t thread2;
+    pthread_t thread3;
+    pthread_t thread4;
+    pthread_t thread5;
 
-    // create threads
-    int nproc = 12; //will be found manually and account for SMT
-    int cpus1[] = {0, 1};
-    create_and_pin_thread(&thread1, extract, pcap_path, nproc, cpus1, 2);
-    int cpus2[] = {5, 6};
-    create_and_pin_thread(&thread2, extract, pcap_path, nproc, cpus2, 2);
+    // spawn threads
+    pthread_create(&thread1, NULL, extract, pcap_path1);
+    pthread_create(&thread2, NULL, extract, pcap_path2);
+    pthread_create(&thread3, NULL, extract, pcap_path3);
+    pthread_create(&thread4, NULL, extract, pcap_path4);
+    pthread_create(&thread5, NULL, extract, pcap_path5);
 
     // join threads
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
+    pthread_join(thread3, NULL);
+    pthread_join(thread4, NULL);
+    pthread_join(thread5, NULL);
 
-    printf("Threads complete \n");
+    
+
+    printf("\nThreads complete \n");
     
 
     return 0;
