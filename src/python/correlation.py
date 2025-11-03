e6e9cef18d4e039373a021faa148898cb8ebdf66
Gabriel Toro <gabrieltoro45@tamu.edu>
2025-11-03 09:21:25 -0600

    added encoding to correlation matrix for testing

--- src/python/correlation.py
+++ src/python/correlation.py
@@ -2,125 +2,40 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from encoding import *
 
-def get_column_names(logfile_type):
-    """
-    Returns the column names corresponding to the indices for each log type.
-    Useful for creating DataFrames with proper column names.
-    
-    Parameters:
-    -----------
-    logfile_type : str
-        Type of log file ('CONN', 'DNS', 'HTTP', 'SSL', 'WEIRD', 'UNKNOWN')
-    
-    Returns:
-    --------
-    list or None : List of column names, or None for UNKNOWN type
-    """
-    
-    logfile_type = logfile_type.upper()
-    
-    if logfile_type == 'CONN':
-        return ['ts', 'uid', 'id.orig_h', 'id.resp_h', 'proto', 'duration', 
-                'orig_bytes', 'resp_bytes', 'conn_state', 'local_orig', 
-                'local_resp', 'orig_pkts', 'orig_ip_bytes', 'resp_pkts', 
-                'resp_ip_bytes']
-    
-    elif logfile_type == 'DNS':
-        return ['ts', 'uid', 'id.orig_h', 'id.resp_h', 'proto', 'rtt', 
-                'query', 'qtype', 'qtype_name', 'rcode', 'rcode_name', 'answers', 'TTLs']
-    
-    elif logfile_type == 'HTTP':
-        return ['ts', 'uid', 'id.orig_h', 'id.resp_h', 'method', 'host', 
-                'uri', 'referrer', 'user_agent', 'request_body_len', 
-                'response_body_len', 'status_code', 'resp_mime_types', 
-                'resp_fuids', 'orig_fuids', 'orig_filenames', 'resp_filenames', 
-                'trans_depth']
-    
-    elif logfile_type == 'SSL':
-        return ['ts', 'uid', 'id.orig_h', 'id.resp_h', 'cipher', 
-                'curve', 'server_name', 'ssl_history']
-    
-    elif logfile_type == 'WEIRD':
-        return ['ts', 'uid', 'id.orig_h', 'id.resp_h', 'name', 'addl', 
-                'notice', 'peer', 'source']
-    
-    elif logfile_type == 'UNKNOWN':
-        return None
-    
-    else:
-        return []
-    
-def get_logtype(path):
-    if 'conn' in path: return 'CONN'
-    if 'dns' in path: return 'DNS'
-    if 'http' in path: return 'HTTP'
-    if 'ssl' in path: return 'SSL'
-    if 'weird' in path: return 'WEIRD'
-    return 'UNKNOWN'
-
-# Load the Zeek datasets
-paths_laptop = [
-    # Node 1 (Child 3)
-    "../benign_1_min/node-1-child-3/csv files/node1_conn.csv",
-    "../benign_1_min/node-1-child-3/csv files/node1_dns.csv",
-    "../benign_1_min/node-1-child-3/csv files/node1_http.csv",
-    "../benign_1_min/node-1-child-3/csv files/node1_ssl.csv",
-    
-    # Node 2 (Child 4)
-    "../benign_1_min/node-2-child-4/csv files/node2_conn.csv",
-    "../benign_1_min/node-2-child-4/csv files/node2_dns.csv",
-    "../benign_1_min/node-2-child-4/csv files/node2_http.csv",
-    "../benign_1_min/node-2-child-4/csv files/node2_ssl.csv",
-    
-    # Camera Pod
-    "../benign_1_min/cam-pod/csv files/camera_conn.csv",
-    
-    # LiDAR Pod
-    "../benign_1_min/lidar-pod/csv files/lidar_conn.csv",
-    
-    # NGINX Pod
-    "../benign_1_min/nginx-pod/csv files/NGINX_conn.csv",
-    "../benign_1_min/nginx-pod/csv files/NGINX_dns.csv"
+conn_paths = [
+    "../../benign_1_min/node-1-child-3/csv files/node1_conn.csv",
+    "../../benign_1_min/node-2-child-4/csv files/node2_conn.csv",
+    "../../benign_1_min/cam-pod/csv files/camera_conn.csv",
+    "../../benign_1_min/lidar-pod/csv files/lidar_conn.csv",
+    "../../benign_1_min/nginx-pod/csv files/NGINX_conn.csv"
 ]
 
-paths_desktop = [
-    # Node 1 (Child 3)
-    "../benign_1_min/Node 1 (Child 3)/csv files/node1_conn.csv",
-    "../benign_1_min/Node 1 (Child 3)/csv files/node1_dns.csv",
-    "../benign_1_min/Node 1 (Child 3)/csv files/node1_http.csv",
-    "../benign_1_min/Node 1 (Child 3)/csv files/node1_ssl.csv",
-    
-    # Node 2 (Child 4)
-    "../benign_1_min/Node 2 (Child 4)/csv files/node2_conn.csv",
-    "../benign_1_min/Node 2 (Child 4)/csv files/node2_dns.csv",
-    "../benign_1_min/Node 2 (Child 4)/csv files/node2_http.csv",
-    "../benign_1_min/Node 2 (Child 4)/csv files/node2_ssl.csv",
-    
-    # Camera Pod
-    "../benign_1_min/Camera Pod/csv files/camera_conn.csv",
-    
-    # LiDAR Pod
-    "../benign_1_min/LiDAR Pod/csv files/lidar_conn.csv",
-    
-    # NGINX Pod
-    "../benign_1_min/NGINX Pod/csv files/NGINX_conn.csv",
-    "../benign_1_min/NGINX Pod/csv files/NGINX_dns.csv"
+dns_paths = [
+    "../../benign_1_min/node-1-child-3/csv files/node1_dns.csv",
+    "../../benign_1_min/node-2-child-4/csv files/node2_dns.csv",
+    "../../benign_1_min/nginx-pod/csv files/NGINX_dns.csv"
 ]
 
-for path in paths_laptop:
+ssl_paths = [
+    "../../benign_1_min/node-1-child-3/csv files/node1_ssl.csv",
+    "../../benign_1_min/node-2-child-4/csv files/node2_ssl.csv"
+]
+
+http_paths = [
+    "../../benign_1_min/node-1-child-3/csv files/node1_http.csv",
+    "../../benign_1_min/node-2-child-4/csv files/node2_http.csv"
+]
+
+# train on 1 of the 5 data sets, and test on the remaining 4
+# do this to see relative strength of training sets
+paths = conn_paths
+
+for path in paths:
     print("****************************************************************************************************")
     print(path)
-    columns = get_column_names(get_logtype(path))
-    df = pd.read_csv(path, na_values=['-'])[columns]
-
-    # Select only numeric columns and exclude timestamp (skews heatmap)
-    df_numeric = df.select_dtypes(include=[np.number])
-    df_numeric = df_numeric.drop(['ts'], axis=1)
-    if(df_numeric.empty):
-        print("Empty dataset")
-        continue
-    print(df_numeric)
+    df_numeric, _ = encode_training_data_conn(path, fit_encoder=True)
 
     # Compute mean vector, covariance matrix, and correlation matrix
     mu = df_numeric.mean()
@@ -143,7 +58,7 @@ for path in paths_laptop:
     axes[1].set_title("Covariance Matrix (Σ)")
 
     # Correlation matrix
-    sns.heatmap(corr_matrix, ax=axes[2], cmap="coolwarm", cbar=True, center=0, annot=True, fmt=".2f")
+    sns.heatmap(corr_matrix, ax=axes[2], cmap="coolwarm", cbar=True, center=0) #, annot=True, fmt=".2f")
     axes[2].set_title("Correlation Matrix (R)")
     axes[2].xaxis.set_ticks_position('bottom')
     axes[2].xaxis.set_label_position('bottom')
